51b2ee733dd0069c304e22259fee1e1e721b0fdb
OroszOliver0311 <oroszoliver20031121@gmail.com>
2026-03-07 10:41:40 +0100

    airplanes table

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AirplaneListComponent } from './components/airplane-list/airplane-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AirplaneListComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
